3779a5f7b9fe9ec4b76f6170cea17654f99e02e7
JúDCraide <juliadcraide31@gmail.com>
2020-08-04 16:07:58 -0300

    Acelerando sua evolução

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles.css'
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm() {
+    return (
+        <div id="page-teacher-form" className="container">
+            <PageHeader
+                title="Que incrível que você quer dar aulas."
+                subtitle="O primeiro passo, é preencher esse formulário de inscrição."
+            />
+        </div>
+    )
+}
+
+export default TeacherForm;
